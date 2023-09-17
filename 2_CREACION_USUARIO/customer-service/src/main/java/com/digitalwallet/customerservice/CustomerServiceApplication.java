0a0c9cdd2a66796520bb0c4e78c5877338b14f49
Luis Antonio Olivera Vasquez <lolivera@bancomercio.com.pe>
2023-09-17 12:06:36 -0500

    Implementacion del API Wallet

--- 2_CREACION_USUARIO/customer-service/src/main/java/com/digitalwallet/customerservice/CustomerServiceApplication.java
+++ 2_CREACION_USUARIO/customer-service/src/main/java/com/digitalwallet/customerservice/CustomerServiceApplication.java
@@ -1,8 +1,13 @@
 package com.digitalwallet.customerservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+@EnableFeignClients("com.digitalwallet.customerservice.infrastructure")
 
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 @SpringBootApplication
 public class CustomerServiceApplication {
 
--- 2_CREACION_USUARIO/customer-service/src/main/java/com/digitalwallet/customerservice/infrastructure/CustomerRepositoryImpl.java
+++ 2_CREACION_USUARIO/customer-service/src/main/java/com/digitalwallet/customerservice/infrastructure/CustomerRepositoryImpl.java
@@ -2,7 +2,12 @@ package com.digitalwallet.customerservice.infrastructure;
 
 import com.digitalwallet.customerservice.domain.Customer;
 import com.digitalwallet.customerservice.domain.CustomerRepository;
+import com.digitalwallet.customerservice.domain.WalletCreation;
+import com.digitalwallet.customerservice.domain.exceptions.DigitalWalletError;
+import com.digitalwallet.customerservice.domain.exceptions.DigitalWalletGenericClientException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,11 +18,24 @@ import java.util.function.Function;
 public class CustomerRepositoryImpl implements CustomerRepository {
 
     private final CustomerMongoRepository mongoRepository;
+    private final WalletAPIRepository walletAPIRepository;
     private final PersistenceMapper persistenceMapper;
 
     @Override
     public Customer register(Customer customer) {
         CustomerDocument customerDocument = mongoRepository.save(persistenceMapper.toDocument(customer));
+        ResponseEntity response = walletAPIRepository.createWallet(new WalletCreation("PEN", customer));
+        if (response.getStatusCode().is4xxClientError()){
+            ResponseEntity<DigitalWalletError> responseError = (ResponseEntity<DigitalWalletError>) response.getBody();
+            throw new DigitalWalletGenericClientException(responseError.getBody().getMessage()
+                    , HttpStatus.valueOf(responseError.getStatusCode().value()));
+        }
+        if (response.getStatusCode().is5xxServerError()){
+            ResponseEntity<DigitalWalletError> responseError = (ResponseEntity<DigitalWalletError>) response.getBody();
+            throw new DigitalWalletGenericClientException(responseError.getBody().getMessage()
+                    , HttpStatus.valueOf(responseError.getStatusCode().value()));
+        }
+
         return persistenceMapper.toDomain(customerDocument);
     }
 
