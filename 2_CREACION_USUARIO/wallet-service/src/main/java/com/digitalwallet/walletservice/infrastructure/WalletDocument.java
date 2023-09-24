06f4da7f9556a4ad352017382fe394ea5775eeda
Luis Olivera <loliverv11@gmail.com>
2023-09-24 16:46:13 -0500

    Se corrigio el caso de uso buscar wallet por numero de telefono

--- 2_CREACION_USUARIO/wallet-service/src/main/java/com/digitalwallet/walletservice/infrastructure/WalletDocument.java
+++ 2_CREACION_USUARIO/wallet-service/src/main/java/com/digitalwallet/walletservice/infrastructure/WalletDocument.java
@@ -1,39 +1,25 @@
 package com.digitalwallet.walletservice.infrastructure;
 
-import com.digitalwallet.walletservice.domain.Customer;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 
+@Getter
 @Document(collection = "wallets")
 public class WalletDocument {
     @Id
     private String walletId;
     private BigDecimal balance;
     private String currency;
-    private Customer customer;
+    private CustomerDocument customer;
 
-    public WalletDocument(String walletId, BigDecimal balance, String currency, Customer customer) {
+    public WalletDocument(String walletId, BigDecimal balance, String currency, CustomerDocument customer) {
         this.walletId = walletId;
         this.balance = balance;
         this.currency = currency;
         this.customer = customer;
     }
 
-    public String getWalletId() {
-        return walletId;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
 }
