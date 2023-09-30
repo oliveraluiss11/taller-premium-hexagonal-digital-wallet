684a49215c40e343f2193b38c302dc47dc076470
Luis Antonio Olivera Vasquez <lolivera@bancomercio.com.pe>
2023-09-30 16:29:03 -0500

    Creacion del microservicio de transfer

---
+++ 4_CREACION_TRANSFERENCIA/transfer-service/src/main/java/com/digitalwallet/transferservice/TransferServiceApplication.java
@@ -0,0 +1,13 @@
+package com.digitalwallet.transferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferServiceApplication.class, args);
+	}
+
+}
