78f7d7a23a5aa97dc4ffae8fbe1f33a2994e3207
Luis Olivera <loliverv11@gmail.com>
2023-10-21 16:40:20 -0500

    FEATURE ADD AUTHENTICATION SERVICE

---
+++ 6_1IMPLEMENTACION_OAUTH/authentication-service/src/test/java/com/digitalwallet/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalwallet.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
